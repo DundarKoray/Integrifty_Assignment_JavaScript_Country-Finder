b542fd57f1201746b27c35e9a6c34ac6b7a6bb0f
Koray Dundar <koraydundar_40@hotmail.com>
2019-03-21 09:45:35 +0200

    js file is created

---
+++ js/countrySearch.js
@@ -0,0 +1,22 @@
+//Get the countries list
+// console.log(countries) // testing if it is working
+
+
+
+//Target the input 'Search Box'
+const searchBox = document.querySelector('.search-box')
+
+// Target the buttons 'Search by Initials' and 'Search by Any'
+const searchByInitials = document.querySelector('.search-by-initials')
+const searchByAny = document.querySelector('.search-by-any')
+
+
+//Search by first letter 
+function searchCountriesByInitials () {
+
+}
+
+//Search is some certain characters exist such as "land"
+function searchCountriesByAny () {
+
+}
